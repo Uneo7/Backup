7e7fb68f5b0a177ef96040563331d0943bfc42f4
Baptiste <Uneo7@users.noreply.github.com>
2018-08-01 13:56:15 +0200

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
 ## Config ##
+
+# Import Swift Config
 source ./swift
 
+# Restic release
 release="0.9.1"
+
+# Repository path
 repo="/"
+
+# Backup location (See Restic doc)
 driver="swift"
 driver_param="Test"
+
+# Folders to backup (separated by comma)
 backup_folders="/home/test;/home/test2"
 
+# Databases to backup (separated by comma)
 databases="test;test2;test3;test3"
 
+# Mysql credentials
 mysql_username="root"
 mysql_password=""
 
-export RESTIC_PASSWORD="test"
+# Restic repository password
+export RESTIC_PASSWORD="changeme"
 
 ## End Config ##
+
 command -v restic > /dev/null 2>&1
 
 if [ $? -gt 0 ]; then
